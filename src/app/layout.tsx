8ce27ad9285073b59c53343abd3cec9f55ad9155
Yousef Ahmed <floky4444@gmail.com>
2025-01-31 21:40:04 +0200

    main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,14 @@ export default function RootLayout({
   }
   return (
     <html lang="ar" dir="rtl">
+        <head>
+        <link rel="icon" href="/images/logo.png" />
+        <link rel="apple-touch-icon" href="/images/logo.png" />
+        <link
+          rel="stylesheet"
+          href="https://site-assets.fontawesome.com/releases/v6.4.2/css/all.css"
+        />
+      </head>
       <body cz-shortcut-listen="false">
         <NavbarMenu />
         {children}
