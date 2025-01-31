c37a65267be4925e06cbc2ffbb64a6cc115b3fc7
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-01-31 20:36:27 +0200

    update header and footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="overlay"></div>
-      <Carousel fade>
+      <Carousel fade interval={3000} controls={false} indicators={false}>
         <Carousel.Item>
           <div className="imgcar">
             <Image src="/images/hero/1.jpeg" fill alt="hero" />
@@ -16,12 +16,26 @@ export default function Hero() {
           <Carousel.Caption>
             <Container>
               <Row>
-                <Col md={6}><h3>First slide label</h3></Col>
-                <Col md={6}><p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Col>
+                <Col md={6}>
+                  <div className="carousel-caption-text ">
+                  <h3>
+                      أول وأكبر منصة إلكترونية تجمع جميع الحدائق العامة الخضراء في
+                      دولتك والعالم
+                  </h3>
+                  <p> 16614 حديقة</p>
+                    </div>
+                  
+                </Col>
+                <Col md={6}>
+                  <Image
+                    src="/images/hero/logo2.png"
+                    alt="hero"
+                    width={323}
+                    height={157}
+                  />
+                </Col>
               </Row>
             </Container>
-            
-            
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -29,8 +43,25 @@ export default function Hero() {
             <Image src="/images/hero/1.jpeg" fill alt="hero" />
           </div>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Container>
+              <Row>
+                <Col md={6}>
+                  <h3>
+                    أول وأكبر منصة إلكترونية تجمع جميع الحدائق العامة الخضراء في
+                    دولتك والعالم
+                  </h3>
+                  <p> 16614 حديقة</p>
+                </Col>
+                <Col md={6}>
+                  <Image
+                    src="/images/hero/logo2.png"
+                    alt="hero"
+                    width={323}
+                    height={157}
+                  />
+                </Col>
+              </Row>
+            </Container>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -38,10 +69,25 @@ export default function Hero() {
             <Image src="/images/hero/1.jpeg" fill alt="hero" />
           </div>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <Container>
+              <Row>
+                <Col md={6}>
+                  <h3>
+                    أول وأكبر منصة إلكترونية تجمع جميع الحدائق العامة الخضراء في
+                    دولتك والعالم
+                  </h3>
+                  <p> 16614 حديقة</p>
+                </Col>
+                <Col md={6}>
+                  <Image
+                    src="/images/hero/logo2.png"
+                    alt="hero"
+                    width={323}
+                    height={157}
+                  />
+                </Col>
+              </Row>
+            </Container>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { Container, Row, Col } from "react-bootstrap";
+import Image from "next/image";
 
 export default function Footer() {
   const pathname = usePathname();
@@ -8,5 +10,45 @@ export default function Footer() {
   if (pathname === "/signin" || pathname === "/signup") {
     return null;
   }
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <Container>
+        <Row>
+          <Col md={5}>
+            <h3>عن ترفيهي</h3>
+            <p>
+              أول وأكبر منصة للحدائق العامة الخضراء.الحدائق هي رئة الحياة التي
+              نتنفس بها المتعة والراحة
+            </p>
+            <p>
+              الرسالة: تقديـــم حلّ شامل في إدارة الترفيه والتسويق والاستثمار
+              للحدائق العامة بمشاركة الإدارة المحلية
+            </p>
+          </Col>
+          <Col md={2}>
+            <h3>حول</h3>
+            <ul>
+              <li>التطبيق</li>
+              <li>حدائقنا</li>
+              <li>من نحن ؟</li>
+              <li>الجهات المشاركه </li>
+              <li>الشركات المشغله </li>
+            </ul>
+          </Col>
+          <Col md={2}>
+            <h3>الموقع </h3>
+            <ul>
+              <li>سياسة الخصوصيه</li>
+              <li>شروط الاستخدام</li>
+              <li>الاخبار والاحداث</li>
+              <li>تواصل معنا !</li>
+            </ul>
+          </Col>
+          <Col md={3}>
+         <Image src={"/images/hero/logo2.png"} alt="logo" width={323} height={157} />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
