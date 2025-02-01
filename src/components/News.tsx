cf241f9a4e5afc12e70034d6376b25c247a09b62
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-01 12:00:03 +0200

    update section

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Slider from "react-slick";
 import Image from "next/image";
 
@@ -20,415 +18,67 @@ export default function News() {
         <Container>
           <Slider {...settings}>
             <div className="box">
-              <Image
-                src={"/images/parks/3.jpeg"}
-                width={250}
-                height={200}
-                alt="logo"
-              />
-              <div className="title">
-                <Container>
-                  <Row>
-                    <Col>
-                      <ul className="d-flex gap-2">
-                        <li>
-                          <FontAwesomeIcon icon={faStar} />
-                        </li>
-                        <li>
-                          <FontAwesomeIcon icon={faStar} />
-                        </li>
-                        <li>
-                          <FontAwesomeIcon icon={faStar} />
-                        </li>
-                        <li>
-                          <FontAwesomeIcon icon={faStar} />
-                        </li>
-                        <li>
-                          <FontAwesomeIcon icon={faStar} />
-                        </li>
-                      </ul>
-                    </Col>
-                    <Col>حديقة متميزه</Col>
-                  </Row>
-                </Container>
+              <div className="box-header d-flex justify-content-between align-items-center">
+                <div className="title">
+                  <h5>حديقة رويال بالاس</h5>
+                  <p>السعودية</p>
+                </div>
+                <div className="header-img">
+                  <Image src={"/images/news/1.jpeg"} fill alt="logo" />
+                </div>
               </div>
-              <div className="rate">
-                <Container>
-                  <Row>
-                    <Col>
-                      <p>200 تقييم</p>
-                    </Col>
-                    <Col> الاسكندرية</Col>
-                  </Row>
-                </Container>
+              <div className="desc">
+                <p className="text-end">
+                  تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                  لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                  وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                  الاحتياجات لهم .
+                </p>
+              </div>
+              <div className="read-more text-start">
+                <p>.. عرض المزيد </p>
               </div>
             </div>
             <div>
               <div className="box">
                 <Image
-                  src={"/images/parks/3.jpeg"}
+                  src={"/images/news/2.jpg"}
                   width={250}
                   height={200}
                   alt="logo"
                 />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
+              </div>
+            </div>
+            <div>
+              <div className="box">
+                <Image
+                  src={"/images/news/3.jpeg"}
+                  width={250}
+                  height={200}
+                  alt="logo"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="box">
+                <div className="box-header">
+                  <Image
+                    src={"/images/news/4.jpg"}
+                    width={250}
+                    height={200}
+                    alt="logo"
+                  />
                 </div>
               </div>
             </div>
             <div>
               <div className="box">
                 <Image
-                  src={"/images/parks/3.jpeg"}
+                  src={"/images/news/5.jpg"}
                   width={250}
                   height={200}
                   alt="logo"
                 />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/parks/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-                <div className="title">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <ul className="d-flex gap-2">
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                          <li>
-                            <FontAwesomeIcon icon={faStar} />
-                          </li>
-                        </ul>
-                      </Col>
-                      <Col>حديقة متميزه</Col>
-                    </Row>
-                  </Container>
-                </div>
-                <div className="rate">
-                  <Container>
-                    <Row>
-                      <Col>
-                        <p>200 تقييم</p>
-                      </Col>
-                      <Col> الاسكندرية</Col>
-                    </Row>
-                  </Container>
-                </div>
               </div>
             </div>
           </Slider>
