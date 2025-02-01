deacf1d0da9b2a6c799bf8ff084348d8d9487239
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-01 17:55:23 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import News from "../components/News";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import AddNewPark from "@/components/AddNewPark";
+import Parks from "@/components/Parks";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <News />
       <FamousParks />
       <AddNewPark />
+      <Parks />
     </>
   );
 }
---
+++ src/components/Parks.tsx
@@ -0,0 +1,3 @@
+export default function Parks() {
+  return <section className="parks">Parks</section>;
+}
