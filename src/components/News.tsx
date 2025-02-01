27d698fc9580a28efeb3e19d895981e81ae6acf7
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-01 12:29:37 +0200

    update

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -13,76 +13,132 @@ export default function News() {
     slidesToScroll: 3,
   };
   return (
-    <section className="news">
+    <section className="news py-5">
       <div className="slider-container text-center mb-5">
         <Container>
           <Slider {...settings}>
-            <div className="box">
-              <div className="box-header d-flex justify-content-between align-items-center">
-                <div className="title">
-                  <h5>حديقة رويال بالاس</h5>
-                  <p>السعودية</p>
+            <div>
+              <div className="box">
+                <div className="box-header d-flex justify-content-between align-items-center">
+                  <div className="title">
+                    <h5>حديقة رويال بالاس</h5>
+                    <p>السعودية</p>
+                  </div>
+                  <div className="header-img">
+                    <Image src={"/images/news/1.jpeg"} fill alt="logo" />
+                  </div>
                 </div>
-                <div className="header-img">
-                  <Image src={"/images/news/1.jpeg"} fill alt="logo" />
+                <div className="desc">
+                  <p className="text-end">
+                    تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                    لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                    وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                    الاحتياجات لهم .
+                  </p>
                 </div>
-              </div>
-              <div className="desc">
-                <p className="text-end">
-                  تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
-                  لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
-                  وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
-                  الاحتياجات لهم .
-                </p>
-              </div>
-              <div className="read-more text-start">
-                <p>.. عرض المزيد </p>
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/news/2.jpg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <Image
-                  src={"/images/news/3.jpeg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="box">
-                <div className="box-header">
-                  <Image
-                    src={"/images/news/4.jpg"}
-                    width={250}
-                    height={200}
-                    alt="logo"
-                  />
+                <div className="read-more text-start">
+                  <p>.. عرض المزيد </p>
                 </div>
               </div>
             </div>
             <div>
               <div className="box">
-                <Image
-                  src={"/images/news/5.jpg"}
-                  width={250}
-                  height={200}
-                  alt="logo"
-                />
+                <div className="box-header d-flex justify-content-between align-items-center">
+                  <div className="title">
+                    <h5>حديقة رويال بالاس</h5>
+                    <p>السعودية</p>
+                  </div>
+                  <div className="header-img">
+                    <Image src={"/images/news/2.jpg"} fill alt="logo" />
+                  </div>
+                </div>
+                <div className="desc">
+                  <p className="text-end">
+                    تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                    لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                    وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                    الاحتياجات لهم .
+                  </p>
+                </div>
+                <div className="read-more text-start">
+                  <p>.. عرض المزيد </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="box">
+                <div className="box-header d-flex justify-content-between align-items-center">
+                  <div className="title">
+                    <h5>حديقة رويال بالاس</h5>
+                    <p>السعودية</p>
+                  </div>
+                  <div className="header-img">
+                    <Image src={"/images/news/3.jpeg"} fill alt="logo" />
+                  </div>
+                </div>
+                <div className="desc">
+                  <p className="text-end">
+                    تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                    لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                    وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                    الاحتياجات لهم .
+                  </p>
+                </div>
+                <div className="read-more text-start">
+                  <p>.. عرض المزيد </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="box">
+                <div className="box-header d-flex justify-content-between align-items-center">
+                  <div className="title">
+                    <h5>حديقة رويال بالاس</h5>
+                    <p>السعودية</p>
+                  </div>
+                  <div className="header-img">
+                    <Image src={"/images/news/4.jpg"} fill alt="logo" />
+                  </div>
+                </div>
+                <div className="desc">
+                  <p className="text-end">
+                    تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                    لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                    وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                    الاحتياجات لهم .
+                  </p>
+                </div>
+                <div className="read-more text-start">
+                  <p>.. عرض المزيد </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="box">
+                <div className="box-header d-flex justify-content-between align-items-center">
+                  <div className="title">
+                    <h5>حديقة رويال بالاس</h5>
+                    <p>السعودية</p>
+                  </div>
+                  <div className="header-img">
+                    <Image src={"/images/news/5.jpg"} fill alt="logo" />
+                  </div>
+                </div>
+                <div className="desc">
+                  <p className="text-end">
+                    تم التعاقد بين أمانة الدمام وشركة الرياض للصيانة والتشغيل
+                    لإدارة العامة بالمشروع التابع للحديقه لتطيور وتجميل الحديقه
+                    وتوفير فاعليات للاطفال اصغر من سن ال7 سنوات وتوفير كافة
+                    الاحتياجات لهم .
+                  </p>
+                </div>
+                <div className="read-more text-start">
+                  <p>.. عرض المزيد </p>
+                </div>
               </div>
             </div>
           </Slider>
-          <div className="read-more mt-5 text-start">
+          <div className="read-more mt-5 text-start ">
             <p>عرض المزيد ..</p>
           </div>
         </Container>
