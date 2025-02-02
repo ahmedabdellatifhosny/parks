c112c67df445d94ab878ef3108936a4ae67dbc72
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-02 19:53:30 +0200

    update

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 export default function Apps() {
   return (
     <section className="apps">
@@ -5,6 +7,38 @@ export default function Apps() {
         <h3 className="text-center">
           تطبيق ترفيهي بارك، استمتع بتجربة فريدة من نوعها كل حدائق العالم بجوالك
         </h3>
+        <div className="apps-frame">
+          <div className="background-box">
+            <Image src={"/images/app/frame.png"} width={680} height={480} alt="app" />
+          </div>
+          <div className="apps-img">
+            <ul className=" list-unstyled d-flex justify-content-center align-items-center gap-3">
+              <li>
+                <p>حمل التطبيق الان </p>
+              </li>
+              <li>
+                <Link href="/">
+                  <Image
+                    src="/images/app/appstore.png"
+                    width={150}
+                    height={50}
+                    alt="appstore"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <Image
+                    src="/images/app/googleplay.png"
+                    width={150}
+                    height={50}
+                    alt="googleplay"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
