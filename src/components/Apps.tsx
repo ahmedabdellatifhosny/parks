a2a66ab37bfb6a18abad2745881a46447a41bdd8
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-03 10:23:26 +0200

    update apps section

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -8,9 +8,14 @@ export default function Apps() {
           تطبيق ترفيهي بارك، استمتع بتجربة فريدة من نوعها كل حدائق العالم بجوالك
         </h3>
         <div className="apps-frame">
-          <div className="background-box">
-            <Image src={"/images/app/frame.png"} width={680} height={480} alt="app" />
-          </div>
+      
+           <div className="app-image">
+            <Image
+              src="/images/app/apps.png"
+             fill
+              alt="app"
+            />
+           </div>
           <div className="apps-img">
             <ul className=" list-unstyled d-flex justify-content-center align-items-center gap-3">
               <li>
